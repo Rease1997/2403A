ff3d7df60d3e01281f83ef8d3655889a7bb075f0
Rease <2714045442@qq.com>
2025-03-24 14:30:10 +0800

    添加忽略列表

---
+++ Assets/Scripts/FrameWork/Boot.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boot : UnitySingleton<Boot>
+{
+    //声音
+    public AudioSource audio;
+
+    //ui三个层级所需要Transform 
+    //三个canvas canvas都是camera渲染类型 通过调节距离控制层级
+    //相机必须是depthonly 并且改正透视类型
+    public Transform BackRoot;
+    public Transform UIRoot;
+    public Transform TipRoot;
+
+
+    void Start()
+    {
+        Init();
+        UIManager.Instance.OpenWindow("GamePanel");
+        //GameScenesManager.Instance.LoadSceneAsync("Game", "PlayerPanel", new string[] { "Girls1", "Girls2", "Girls3" });
+    }
+
+    void Init()
+    {
+        gameObject.AddComponent<GameScenesManager>().Init(this);
+        UIManager.Instance.Init(TipRoot, UIRoot, BackRoot);
+    }
+
+    public void ChangeAudio(AudioClip clip)
+    {
+        audio.clip = clip;
+        audio.time = 0;
+        audio.Play();
+    }
+}
---
+++ Assets/Scripts/SphereMove.cs
@@ -0,0 +1,39 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMove : MonoBehaviour
+{
+    public Transform cube;
+    //Vector3[] paths;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //paths = null;
+        //Transform pathsParent = GameObject.Find("Paths").transform;
+        //paths = new Vector3[pathsParent.childCount];
+        //for (int i = 0; i < pathsParent.childCount; i++)
+        //{
+        //    paths[i] = pathsParent.GetChild(i).position;
+        //}
+        DoMoveFunc();
+    }
+
+    public void DoMoveFunc()
+    {
+        //transform.DOPath(paths, 30f);
+        //transform.DOMove(Vector3.zero, 3f);
+    }
+
+    public void Update()
+    {
+        //点乘计算前后
+        //dot传两个参数必须是向量(transform.forward, cube.position.normalized)
+        //得到点乘结果计算反余弦值(Mathf.Acos)
+        //再乘以弧度到度(Mathf.Rad2Deg)
+        float angle = Mathf.Acos(Vector3.Dot(transform.forward, cube.position.normalized)) * Mathf.Rad2Deg;
+
+        Debug.Log(angle);
+    }
+}
